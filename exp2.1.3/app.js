8e5a42d3fd47a4e045e0adfa2b4ca39def3ded34
ArpitSharma-code <24bai70854@cuchd.in>
2026-03-08 23:24:14 +0530

    Update app.js

--- exp2.1.3/app.js
+++ exp2.1.3/app.js
@@ -19,8 +19,8 @@ app.get("/", (req,res)=>{
 res.redirect("/products");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
 console.log("Server running on port", PORT);
 });
